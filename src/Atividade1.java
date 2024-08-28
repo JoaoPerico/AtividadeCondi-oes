6431b0aad0b5fc1a83a3e94c4ad3abbed71b7c7a
osvaldinho11 <joaovitorperico@gmail.com>
2024-08-28 20:23:02 -0300

    Atividades

--- src/Atividade1.java
+++ src/Atividade1.java
@@ -1,8 +1,15 @@
 public class Atividade1 {
     public static void main(String[] args) {
 
+Integer idade = 19;
 
 
+
+if (idade >= 18) {
+    System.out.println("Maior de idade");
+}else if (idade < 18) {
+    System.out.println("Menor de idade");
+}
 
 
     }
---
+++ src/Atividade2.java
@@ -0,0 +1,18 @@
+public class Atividade2 {
+    public static void main(String[] args) {
+
+        Integer nota = 9;
+
+        if (nota >= 9) {
+            System.out.println("Exelente");
+        }else if (nota >= 7) {
+            System.out.println("Bom");
+        }else if (nota >= 5) {
+            System.out.println("Satisfatorio");
+        }else if (nota < 5) {
+            System.out.println("Insatisfatorio");
+        }
+
+
+    }
+}
